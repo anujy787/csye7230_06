7bbd3c26ff5497fd418d75aa30e7ac197289cd5c
Sangeet <sangeetsaha1116@gmail.com>
2024-02-09 19:10:31 -0500

    database config

--- app/models.py
+++ app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class SampleModel(models.Model):
+    # CharField for a short text field
+    name = models.CharField(max_length=100)
+
+    # IntegerField for an integer field
+    age = models.IntegerField()
+
+    # EmailField for storing email addresses
+    email = models.EmailField()
+
+    # DateField for a date
+    birthdate = models.DateField()
+
+    # BooleanField for a boolean value
+    is_active = models.BooleanField(default=True)
+
+    # TextField for a longer text field
+    description = models.TextField(blank=True, null=True)
+
+    # DateTimeField for date and time
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        # Specify the table name for the model
+        db_table = 'user_model'
--- venture_verse/settings.py
+++ venture_verse/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from configparser import ConfigParser
+config = ConfigParser()
+config.read("config.ini")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +78,17 @@ WSGI_APPLICATION = 'venture_verse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': config.get("database","database"),
+		'USER': config.get("database","user"),
+		'PASSWORD': config.get("database","password"),
+		'HOST': config.get("database","host"),
+		'PORT': config.get("database","port"),
+	}
 }
 
 

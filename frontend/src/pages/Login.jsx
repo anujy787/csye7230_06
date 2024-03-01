640bc6e9b9c6acab4ea9840d39a4a0ad59d84cf6
Sangeet <sangeetsaha1116@gmail.com>
2024-03-01 18:05:18 -0500

    minor fix

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Login = () => {
 
   const [error, setError] = React.useState('')
-  
+
   const handleLogin = () => {
     
   }
@@ -13,13 +13,12 @@ const Login = () => {
   }
   return (
     <div className='login'>
-        <h1>Student Login</h1>
+        <h1>Login</h1>
         <h2>{error}</h2>
         <input type="text" placeholder='ID' onChange={handleChange} name='id' required/>
         <input type="password" placeholder='Password' onChange={handleChange} name='password' required/>
         <button onClick={()=>handleLogin()}>Login</button>
-        <p><Link to="/adminlogin">Are you an admin?</Link></p>
-        <p><Link to="/registration">Are you a new student?</Link></p>
+        <p><Link to="/register">Are you a new explorer?</Link></p>
     </div>
   )
 }

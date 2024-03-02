fa77d4d05723ea904f33bddb0a50797d628170f4
Anuj Sharma <sharma.anujy@northeastern.edu>
2024-03-01 19:00:25 -0500

    implemented healthz api

---
+++ healthz/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import HealthzView
+
+urlpatterns = [
+    path("", HealthzView.as_view(), name="api_check"),
+]
--- venture_verse/urls.py
+++ venture_verse/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/user", include("app.urls")),
+    path("healthz/", include("healthz.urls")),
+    
 ]

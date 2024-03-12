903d780d2e1ffbfefae991b876d39a33a397bf4e
Anuj Sharma <sharma.anujy@northeastern.edu>
2024-03-12 12:43:58 -0400

    working logout

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import RegisterView, LoginView
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("v1/user", RegisterView.as_view()),
     path("v1/user/self", LoginView.as_view()),
     path('', views.home, name='home'),
     path('login/', views.login, name='login'), 
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+

791b7bca29faa3b5d425ed03eee1ad9958731cb2
Anuj Sharma <sharma.anujy@northeastern.edu>
2024-04-07 23:30:35 -0400

    update travel plan

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, TravelPlanCreateView, TravelPlanUpdateView
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("v1/user", RegisterView.as_view(), name="reg"),
     path("v1/user/self", LoginView.as_view(), name="log"),
+    path("v1/plan", TravelPlanCreateView.as_view(), name="create-travel-plan"),
+    path("v1/plan/self", TravelPlanUpdateView.as_view(), name="create-travel-plan"),
+    path(
+        "v1/plan/self/<int:pk>",
+        TravelPlanUpdateView.as_view(),
+        name="update-travel-plan",
+    ),
     path("", views.home, name="home"),
     path("login/", views.login, name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),

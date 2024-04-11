bd8f3f18b8129e47f64d42549bbbfc15175873ed
Anuj Sharma <sharma.anujy@northeastern.edu>
2024-04-11 14:34:34 -0400

    fix superuser

--- app/models.py
+++ app/models.py
@@ -4,33 +4,24 @@ import uuid
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(
-        self, email, username, first_name, last_name, password=None, **extra_fields
-    ):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
         if not email:
-            raise ValueError("The Email field must be set")
+            raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            **extra_fields,
-        )
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(
-        self, email, first_name, last_name, password=None, **extra_fields
-    ):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(email, first_name, last_name, password, **extra_fields)
+    
 
 
 class User(AbstractUser):
@@ -44,8 +35,9 @@ class User(AbstractUser):
     rating = models.DecimalField(max_digits=3, decimal_places=2, default=5.00)
     is_subscribed = models.BooleanField(default=False)
     bio = models.CharField(max_length=150, blank=True, default="")
+    username = None
 
-    USERNAME_FIELD = "username"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = CustomUserManager()

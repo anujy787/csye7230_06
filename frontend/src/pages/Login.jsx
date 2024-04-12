30ea1a268ef412cc88f1583aa028a6bf4604c1e0
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2024-04-12 15:04:39 -0400

    Login test case

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,72 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import './Login.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
 import Lottie from 'lottie-react';
 import Animation from '../assets/anim2.json';
+
 /**
  * Represents the Login component.
  * Handles user login and authentication.
- * 
+ *
  * @returns {JSX.Element} A React element.
  */
 
 const Login = () => {
-
   const navigate = useNavigate();
 
-  const [error, setError] = React.useState('')
+  const [error, setError] = React.useState('');
   const [user, setUser] = React.useState({
     email: '',
-    password: ''
-  })
-
+    password: '',
+  });
 
   /**
    * Handles user login.
    */
 
   const handleLogin = async () => {
-    try{
+    try {
       const response = await axios.get('http://localhost:8000/v1/user/self', {
         auth: {
           username: user.email,
-          password: user.password
-        }
+          password: user.password,
+        },
       });
-      if(response.status === 200) {
-        sessionStorage.setItem('auth', JSON.stringify({ username: user.email, password: user.password }));
-        navigate("/", { replace: true });
+      if (response.status === 200) {
+        sessionStorage.setItem(
+          'auth',
+          JSON.stringify({ username: user.email, password: user.password })
+        );
+        navigate('/', { replace: true });
       }
-    } 
-    catch(err) {
-      alert(err)
+    } catch (err) {
+      alert(err);
+      setError(err.message);
     }
-    
-  }
+  };
+
   const handleChange = (e) => {
+    console.log(e.target.value);
     setUser({
       ...user,
-      [e.target.name]: e.target.value
-    })
-  }
-
+      [e.target.name]: e.target.value,
+    });
+  };
 
   /**
    * Handles Google login.
    */
 
-  const handleGoogleLogin = async () => {
+  const handleGoogleLogin = async () => {};
 
-  }
   return (
     <div className="page-container">
       <div className="animation-container">
-      <div style={{ width: '300px', height: '300px' }}>
+        <div style={{ width: '300px', height: '300px' }}>
           <Lottie animationData={Animation} loop={true} />
-     </div>
-     </div>
+        </div>
+      </div>
       <div className="login-container">
         <h1 className="login-header">Login</h1>
         <h2 className="login-error">{error}</h2>
@@ -97,7 +98,7 @@ const Login = () => {
         <p className="login-link">
           <Link to="/register">Are you a new explorer?</Link>
         </p>
-        
+
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <p className="forgot-password">
             <Link to="/forgot-password">Forgot Password</Link>
@@ -109,6 +110,6 @@ const Login = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
